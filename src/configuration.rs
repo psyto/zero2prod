41ceeb71cb76348dd326577ade54053450fe3f0c
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2024-01-29 17:15:41 +0900

    added postgres connection test to health_check.rs

--- src/configuration.rs
+++ src/configuration.rs
@@ -25,3 +25,27 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     // Try to convert the configuration values it read into our Settings type
     settings.try_deserialize::<Settings>()
 }
+
+impl DatabaseSettings {
+    // We're using a PostgreSQL database, so we need to build a connection string
+    // that looks like this:
+    // postgresql://<username>:<password>@<host>:<port>/<database-name>
+    // We're using the postgres crate to help us build the connection string.
+    // We're also using the once_cell crate to make this a lazy static.
+    // This means it will be initialized the first time it's used, and will then
+    // hold onto the value so we don't need to recompute it every time.
+    pub fn connection_string(&self) -> String {
+        // We're using the postgres crate to help us build the connection string.
+        // We're also using the once_cell crate to make this a lazy static.
+        // This means it will be initialized the first time it's used, and will then
+        // hold onto the value so we don't need to recompute it every time.
+            format!(
+                "postgresql://{}:{}@{}:{}/{}",
+                self.username,
+                self.password,
+                self.host,
+                self.port,
+                self.database_name
+            )
+    }
+}
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,7 @@
+//! tests/health_check.rs
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
-use zero2prod::startup::run;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
@@ -34,6 +36,13 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    // The `Connection` trait MUST be in scope for us to invoke
+    // `PgConnection::connect` - it is not an inherent method of the struct!
+    let connection = PgConnection::connect(&connection_string)
+    .await
+    .expect("Failed to connect to Postgres");
     let client = reqwest::Client::new();
 
     // Act
